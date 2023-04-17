07dccfbfcce2beae181cbfd8d595b24618609727
EEErinzzz <116625261+EEErinzzz@users.noreply.github.com>
2023-04-17 16:45:30 +0800

    Update prim.h

--- prim_algorithm/prim.h
+++ prim_algorithm/prim.h
@@ -10,21 +10,24 @@ class maze_generation
 {
 public:
 	void maze_generator();
-	//use prim algorithm to generator the maze
+		//use prim algorithm to generator the maze
 
 	void maze_printer();
-	//print the maze
+		//print the maze
 
 	maze_generation(int s);
-	// a constructor 
-	// initialize all grids to be the wall
-	// set the outmost periphery as the land
+		// a constructor 
+		// initialize all grids to be the wall
+		// set the outmost periphery as the land
 private:
 	int size;
 	vector<vector<int>> maze;
+		//maze is a 2-D vector
 	int wall = 0;
 	int land = 1;
 	vector<PII> frontiers;
+		// frontiers is the up, down, left, right grid that is wall around the current grid
+		// all frontiers are saved in this vector
 
 };
 
