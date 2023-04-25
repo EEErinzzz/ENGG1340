13e4e14edb05d6eab40736181a8a1102a48910ec
whiteqqqq <124782500+whiteqqqq@users.noreply.github.com>
2023-04-25 22:34:49 +0800

    Update File IO.cpp

--- File IO.cpp
+++ File IO.cpp
@@ -4,25 +4,55 @@
 #include<algorithm>
 #include "rank.h"
 using namesapce std;
-bool SortByTime(rank a, rank b);
+bool SortByTime(Record a, Record b);
+void Addrecord(string name, int time);
 void Insertrecord(string name, int time);
 void Display();
 struct Record rank[5];
 
-int main()
+int main() // read the output from other cpp files and write it to the txt file
 {
-  
+   
+}
+int rank(string name, int time)
+{
+	fstream fin("rank_classic.txt",ios::in)
 }
 
-
-
-bool SortByTime(rank a, rank b)
+bool SortByTime(Record a, Record b)
 {
 	if( a.TimeUsed != b.TimeUsed)
 		return a.TimeUsed < b.TimeUsed;
 }
+void Addrecord(string name, int time) // in case there are less than 5 players play the game
+{
+	fstream fin("rank_classic.txt",ios::in);
+	ofstream fout("rank_classic.txt");
+	char c;
+	int line = 0;
+	while(fin.get(c))
+	{
+		if(c == "\n")
+			line++;
+	}
+    if(line<=1)
+	{
+			rank[line].TimeUsed=time;
+			rank[line].PlayerName=name;
+	}
+	elseif(line>1)
+	{
+			rank[line].TimeUsed=time;
+			rank[line].PlayerName=name;
+			sort(rank, rank +line, SortByTime);
+			for (int i = 0; i < line;i++){
+				fout <<i+1<<" "<<rank[i].PlayerName <<" "<<rank[i].TimeUsed<<endl;					
+	}
+	fin.close();
+	fout.close();
+}
 
-void Insertrecord(string name, int time)
+void Insertrecord(string name, int time) // check and adjust the rank
 {
 	ofstream fout;
 	fout.open("rank_Classic.txt");
