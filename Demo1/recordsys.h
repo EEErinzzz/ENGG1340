88b4fcde4b566fcf98125a8f7c3113467e2bf44c
linearalgebrayhz <121793124+linearalgebrayhz@users.noreply.github.com>
2023-04-29 16:09:23 +0800

    Create recordsys.h

---
+++ Demo1/recordsys.h
@@ -0,0 +1,23 @@
+#ifndef RECORDSYS_H
+#define RECORDSYS_H
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+/*
+struct Record
+{
+	std::string GameMode;
+	std::string PlayerName;
+	int TimeUsed;
+	int size;
+};
+*/
+
+bool SortByTime(Record a, Record b);
+void Insertrecord(string name, int time);
+void Display();
+void k(Record &a);
+
+#endif
