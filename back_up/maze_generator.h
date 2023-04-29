de4deaa54f09c23a9c2ba12ce23e79cc922c8b77
EEErinzzz <116625261+EEErinzzz@users.noreply.github.com>
2023-04-29 16:25:45 +0800

    Create maze_generator.h

---
+++ back_up/maze_generator.h
@@ -0,0 +1,35 @@
+#include <vector>
+using namespace std;
+
+#ifndef Gernerator_H
+#define Generator_H
+
+typedef pair<int, int> PII;
+
+class maze_generation
+{
+public:
+	void maze_generator();
+	//use prim algorithm to generator the maze
+
+	void maze_printer();
+	//print the maze
+
+	maze_generation(int s);
+	// a constructor 
+	// initialize all grids to be the wall
+	// set the outmost periphery as the land
+	void player_insertion(char player, int y, int x);
+	void update_player_location(int y1, int x1, int y2, int x2); //This function will update the player's location on the maze
+	bool check_wall(int y, int x);
+private:
+	int size;
+	vector<vector<int>> maze;
+	int wall = 0;
+	int land = 1;
+	vector<PII> frontiers;
+
+};
+
+
+#endif
