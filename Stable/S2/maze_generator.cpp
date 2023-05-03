c1dd75b85beb300ab2a2c108b19cfdfda60c64c3
OneCodingNewbie <125623276+OneCodingNewbie@users.noreply.github.com>
2023-05-03 20:55:25 +0800

    Add files via upload

---
+++ Stable/S2/maze_generator.cpp
@@ -0,0 +1,159 @@
+#include "maze_generator.h"
+#include<time.h>
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <ncurses.h>
+
+using namespace std;
+maze_generation::maze_generation(int s)
+{
+	//initialize all grids in the maze as wall
+	size = s;
+	maze.resize(size);
+	for (int j = 0; j < size; j++)
+	{
+		maze[j].resize(size);
+		for (int i = 0; i < size; i++)
+		{
+
+			maze[j][i] = wall;
+		}
+	}
+	// set the outmost periphery as the land to prevent from overriding the boundary
+	for (int i = 0; i < size; i++) {
+		maze[i][0] = 1;
+		maze[0][i] = 1;
+		maze[size - 1][i] = 1;
+		maze[i][size - 1] = 1;
+	}
+
+}
+
+void maze_generation::maze_generator(string command)
+{
+	frontiers.push_back({ 2,2 });
+	// randomly choose a grid as the initial point
+
+	while (frontiers.size())
+	{
+		srand(time(0));
+		int r = rand() % frontiers.size();
+		int cur_y = frontiers[r].first;
+		int cur_x = frontiers[r].second;
+		
+		// judge whether the surounding(up,down,left and right) of cur_ is connected by land
+		int count = 0;
+		for (int i = cur_y - 1; i < cur_y + 2; i++) {
+			for (int j = cur_x - 1; j < cur_x + 2; j++) {
+				if (abs(cur_y - i) + abs(cur_x - j) == 1 && maze[i][j] > 0) {
+					++count;
+				}
+			}
+		}
+		
+		if (count <= 1)
+		{
+			maze[cur_y][cur_x] = 1;
+			// make it the land
+
+			// add the frontiers of the new land to the vector frontiers
+			for (int i = cur_y - 1; i < cur_y + 2; i++)
+			{
+				for (int j = cur_x - 1; j < cur_x + 2; j++)
+				{
+					if (abs(cur_y - i) + abs(cur_x - j) == 1 && maze[i][j] == 0) frontiers.push_back({ i,j });
+				}
+			}
+		}
+		
+		frontiers.erase(frontiers.begin()+r);
+			// since it is already marked as land, it should be removed from the frontiers vector
+	}
+	//set the exit and entry in classic mode
+	if (command == "open")
+	{ 
+		maze[2][1] = 1;
+		for (int i = size - 3; i >= 0; i--) {
+			if (maze[i][size - 3] == 1)
+			{
+				maze[i][size - 2] = 1;
+				break;
+			}
+		}
+	}
+	
+}
+
+void maze_generation::maze_printer()
+{
+	for (int i = 0; i < size; i++) 
+	{
+		for (int j = 0; j < size; j++)
+		{
+			if (maze[i][j] == 'X') { printw("X"); refresh(); continue; }
+			if (maze[i][j] == 1) { printw(" "); refresh(); }
+			else { addch(' ' | A_REVERSE); } ;
+		}
+		printw("\n");
+		refresh();
+	}
+}
+void maze_generation::maze_printer_Foggy(int sight)
+//input is the sight that the player have. Player can only see the maze within a circle whose radius is the sight and the center is the player position
+{
+	for (int i = 1; i < size; i++)
+	{
+		for (int j = 1; j < size - 1; j++)
+		{
+			//print the boundary of the maze
+			if ((maze[i][j] != 1 && j == size - 2) || (maze[i][j] != 1 && i == 1) || (maze[i][j] != 1 && i == size - 2) || (maze[i][j] == 0 && j == 1))
+			{
+				addch(' ' | A_REVERSE);
+
+
+			}
+			else if (abs(i - player_y) * abs(i - player_y) + abs(j - player_x) * abs(j - player_x) <= sight * sight && maze[i][j] == 0)
+			{
+				addch(' ' | A_REVERSE);
+			}
+			else if (maze[i][j] == 'X') { printw("X"); refresh(); continue; }
+			else
+			{
+				printw(" "); refresh();
+			}
+		}
+		printw("\n");
+		refresh();
+	}
+}
+			
+void maze_generation::player_insertion(char player, int y, int x)
+{
+	maze[y][x] = player;
+		//maze: a int vector: char player --(casting)--> int
+	player_x = x;
+	player_y = y;
+}
+
+void maze_generation::update_player_location(int y1, int x1, int y2, int x2)
+{
+	maze[y1][x1] =  1;
+	maze[y2][x2] = 'X';
+	player_x = x2;
+	player_y = y2;
+}
+
+bool maze_generation::check_wall(int y, int x)
+{
+	if (maze[y][x] == 1)
+		return true;
+	else
+		return false;
+}
+void maze_generation::just_update_player(int y1, int x1, int y2, int x2)
+{
+	mvaddch(y1, x1, ' ');
+	mvaddch(y2, x2, 'X');
+}
+
---
+++ Stable/S2/rank.h
@@ -0,0 +1,12 @@
+#ifndef RANK_H
+#define RANK_H
+#include<iostream>
+#include<string>
+struct Record
+{
+	std::string GameMode;
+	std::string PlayerName;
+	int TimeUsed;
+	int size;
+};
+#endif
